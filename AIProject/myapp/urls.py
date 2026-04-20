9010b08faa37707cb69299e40abe4dc50db9e00c
LouisWorld-HPT <102909727+LouisWorld-HPT@users.noreply.github.com>
2026-04-20 03:03:46 +0100

    Dataset feyzazkefe/trashnet

--- AIProject/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views 
-
-urlpatterns = [
-    path("home/", views.Home, name="Home"),
-    path("about/", views.About, name="About"),
-    path("team/", views.Team, name="Team"),
-   
-]
